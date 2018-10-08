8733130581eff5fdee1ac6a148efa2fe3bd35380
Skylar Salerno <skylar.salerno@gmail.com>
2018-10-08 18:37:06 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -4,9 +4,9 @@ def prime?(num)
   factors=(2...num).to_a
   puts factors
   factors.each do |number|
-  if(num.fdiv(number)==(num/number)
-    prime=0
-  end
+    if(num.fdiv(number)==(num/number)
+     prime=0
+    end
   end
 puts prime
 end
