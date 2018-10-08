d444081d8dc921d42c8d8268b1cb26e0ad700117
Skylar Salerno <skylar.salerno@gmail.com>
2018-10-08 18:46:45 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 # Add  code here!
 def prime?(num)
   prime=TRUE
-  factors=(2...(num.abs)).to_a
+  factors=(1...(num.abs)).to_a
   puts factors
   factors.each do |number|
     if num.fdiv(number)==(num/number)
